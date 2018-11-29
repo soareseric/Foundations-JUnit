dc64553c63a2c15d51a1375aae3341792371c644
Eric Soares Maciel <emaciel@brq.com>
2018-11-29 10:07:55 -0200

    Organizacao dos arquivos de test

---
+++ src/test/java/br/com/ericsoares/services/LocacaoServiceTest.java
@@ -0,0 +1,32 @@
+package br.com.ericsoares.services;
+
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.ericsoares.entities.Filme;
+import br.com.ericsoares.entities.Locacao;
+import br.com.ericsoares.entities.Usuario;
+import br.com.ericsoares.utils.DataUtils;
+
+public class LocacaoServiceTest {
+
+	@Test
+	public void test() {
+		// cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+
+		// acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
+
+		// verificacao
+		Assert.assertTrue(locacao.getValor() == 5.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+	}
+
+}
