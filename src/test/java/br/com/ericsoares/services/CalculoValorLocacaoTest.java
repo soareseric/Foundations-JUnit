bd97ae3c70479062b5d04718f1dd2c422fc7b8f7
soareseric <contato.eric.sm@gmail.com>
2018-11-29 16:48:46 -0200

    Testes parametrizaveis

---
+++ src/test/java/br/com/ericsoares/services/CalculoValorLocacaoTest.java
@@ -0,0 +1,75 @@
+package br.com.ericsoares.services;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import br.com.ericsoares.entities.Filme;
+import br.com.ericsoares.entities.Locacao;
+import br.com.ericsoares.entities.Usuario;
+import br.com.ericsoares.exceptions.FilmeSemEstoqueException;
+import br.com.ericsoares.exceptions.LocadoraException;
+
+// ESSA ANNOTATION DEFINE O MÉTODO QUE O JUNIT IRÁ ANALIZAR SEUS TESTES
+@RunWith(Parameterized.class)
+public class CalculoValorLocacaoTest {
+
+	private LocacaoService service;
+
+	@Parameter
+	public List<Filme> filmes;
+
+	@Parameter(value=1) // ESSA ANNOTATION DEFINI O VALOR DO PARAMETRO VALORLOCACAO
+	public Double valorLocacao;
+	
+	@Parameter(value=2) // ESSA ANNOTATION DEFINI O VALOR DO PARAMETRO CENARIO
+	public String cenario;
+
+	@Before
+	public void setup() {
+		service = new LocacaoService();
+	}
+	
+	private static Filme filme1 =  new Filme("Filme 1", 1, 4.0); // INSTANCIANDO OS FILMES EM VARIAVEIS
+	private static Filme filme2 =  new Filme("Filme 2", 1, 4.0);
+	private static Filme filme3 =  new Filme("Filme 3", 1, 4.0);
+	private static Filme filme4 =  new Filme("Filme 4", 1, 4.0);
+	private static Filme filme5 =  new Filme("Filme 5", 1, 4.0);
+	private static Filme filme6 =  new Filme("Filme 6", 1, 4.0);
+	private static Filme filme7 =  new Filme("Filme 7", 1, 4.0);
+	
+	@Parameters(name="{2}") // O PARAMETRO PASSADO NESSA ANNOTATION DEFINI O VALOR A SER UTILIZADO COMO DEFINIÇÃO DE CADA TESTE
+	// NESSE CASO, SERÁ PASSADO O VALOR QUE A VARIAVEL CENARIO POSSUIR
+	public static Collection<Object[]> getParametros(){ 
+		return Arrays.asList(new Object[][] {
+			{Arrays.asList(filme1, filme2), 8.0, "2 Filmes: Sem desconto"},
+			{Arrays.asList(filme1, filme2, filme3), 11.0, "3 Filmes: 25%"},
+			{Arrays.asList(filme1, filme2, filme3, filme4), 13.0, "4 Filmes: 50%"},
+			{Arrays.asList(filme1, filme2, filme3, filme4, filme5), 14.0, "5 Filmes: 75%"},
+			{Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6), 14.0, "6 Filmes: 100%"},
+			{Arrays.asList(filme1, filme2, filme3, filme4, filme5, filme6, filme7), 18.0, "7 Filmes: Sem desconto"},
+		}); 
+	}
+
+	@Test
+	public void deveCalcularValorLocacaoConsiderandoDescontos() throws FilmeSemEstoqueException, LocadoraException {
+		// ETAPA 1 : CENARIO
+		Usuario usuario = new Usuario("Usuario 1");
+
+		// ETAPA 2 : AÇÃO
+		Locacao resultado = service.alugarFilme(usuario, filmes);
+
+		// ETAPA 3 : VERIFICAÇÃO
+		assertThat(resultado.getValor(), is(valorLocacao));
+	}
+}
