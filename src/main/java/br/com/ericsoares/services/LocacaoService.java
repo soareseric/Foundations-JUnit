d66ee104230ddf537583af198b01e9a7b621df7f
Eric Soares Maciel <emaciel@brq.com>
2018-11-29 09:48:46 -0200

    Iniciando os testes

---
+++ src/main/java/br/com/ericsoares/services/LocacaoService.java
@@ -0,0 +1,50 @@
+package br.com.ericsoares.services;
+
+import static br.com.ericsoares.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.ericsoares.entities.Filme;
+import br.com.ericsoares.entities.Locacao;
+import br.com.ericsoares.entities.Usuario;
+import br.com.ericsoares.utils.DataUtils;
+
+public class LocacaoService {
+
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		// Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+
+		// Salvando a locacao...
+		// TODO adicionar método para salvar
+
+		return locacao;
+	}
+
+	@Test
+	public void test() {
+		// cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+
+		// acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
+
+		// verificacao
+		Assert.assertTrue(locacao.getValor() == 5.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+	}
+}
